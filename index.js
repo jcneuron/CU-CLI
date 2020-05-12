cce6f1afbb192c3d4925ed06d9244b60790694af
Juan Uribe <dijonmylo@gmail.com>
2020-05-11 21:09:55 -0400

    working yargs
    lol

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//refers to yargs module
+const yargs = require('yargs');
+
 //refers to js file
 const postTask = require('./clickup.js');
 
@@ -6,22 +9,48 @@ const axios = require('axios');
 
 //POST request url --> https://jsapi.apiary.io/apis/clickup20/reference/0/tasks/create-task.html
 //link to use:            https://api.clickup.com/api/v2/list/list_id/task
-//list_id = 19126702 from https://app.clickup.com/1295328/v/li/19126702
-const url = 'https://api.clickup.com/api/v2/list/19126702/task';
+//list_id = 25214909 from https://app.clickup.com/2331408/v/l/li/25214909
+const url = 'https://api.clickup.com/api/v2/list/25214909/task';
 
-//API token in seetings->Apps->generate
+//API token in settings->Apps->generate
 //https://jsapi.apiary.io/apis/clickup20/introduction/authentication.html
 const token = 'pk_4235004_1QFR20XOHXEFJ4ZHXN2JYOVQFW0OQF56';
 
-//body properties from https://jsapi.apiary.io/apis/clickup20/reference/0/tasks/create-task.html
-// Production / JavaScript
+// body properties from https://jsapi.apiary.io/apis/clickup20/reference/0/tasks/create-task.html
+// Production / JavaScript;
 var body = {
-	name: 'New Task Name',
-	content: 'New Task Content',
-	tags: [ 'tag name 1' ],
-	status: 'Open',
+	name: 'taskName',
+	content: 'taskDesc',
+	tags: [ 'taskTag' ],
+	status: 'taskStatus',
 	priority: 3
 };
 
+yargs
+	.command({
+		command: 'add <taskName> <taskDesc>',
+		aliases: [ 'create' ],
+		desc: 'Create a new task on ClickUp',
+		// builder: {
+		// 	body: {
+		// 		name: 'hi',
+		// 		status: 'Open'
+		// 	}
+		// },
+		handler: (argv) => {
+			// console.log('Created task ${argv.taskName} with description ${argv.taskDesc}');
+			body.name = argv.taskName;
+			body.content = argv.taskDesc;
+			// body.tags = argv.taskTag;
+			// body.status = argv.taskStatus;
+			// body.priority = argv.num;
+			postTask(url, body, token);
+		}
+	})
+	.demandCommand(1, 'Please choose at least one command')
+	.help()
+	.wrap(72).argv;
+
 //references and passes params to the function in index.js
-postTask(url, body, token);
+
+// yargs.parse();
